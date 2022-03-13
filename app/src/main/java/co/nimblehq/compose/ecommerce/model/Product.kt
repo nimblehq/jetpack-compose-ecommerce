83dc6f3a91b0848af304d270c3c86dc9f0005759
hoangnguyen92dn <hoang.l@nimblehq.co>
2022-03-13 16:22:11 +0700

    [#9] Add list of New Arrivals products section to Home screen

--- app/src/main/java/co/nimblehq/compose/ecommerce/model/Product.kt
+++ app/src/main/java/co/nimblehq/compose/ecommerce/model/Product.kt
@@ -15,12 +15,27 @@ val mockPopularProducts = listOf(
                 id = 1,
                 name = "Pink Cube",
                 price = 8000,
-                image = R.drawable.ic_shirt
+                image = R.drawable.ic_shirt_pink_cube
         ),
         Product(
                 id = 2,
                 name = "Honey Ico",
                 price = 5000,
-                image = R.drawable.ic_shirt
+                image = R.drawable.ic_shirt_honey_ico
+        )
+)
+
+val mockNewArrivalsProducts = listOf(
+        Product(
+                id = 1,
+                name = "Indigo Shuzan",
+                price = 8000,
+                image = R.drawable.ic_shirt_indigo_shuzan
+        ),
+        Product(
+                id = 2,
+                name = "Gray Taurus",
+                price = 5000,
+                image = R.drawable.ic_shirt_gray_taurus
         )
 )
