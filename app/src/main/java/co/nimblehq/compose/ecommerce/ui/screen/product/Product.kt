061c396e20a0ae60c260bf86d4eb83957ce690d7
hoangnguyen92dn <hoang.l@nimblehq.co>
2022-03-30 23:00:34 +0700

    [#18] Add empty cart page

--- app/src/main/java/co/nimblehq/compose/ecommerce/ui/screen/product/Product.kt
+++ app/src/main/java/co/nimblehq/compose/ecommerce/ui/screen/product/Product.kt
@@ -1,19 +1,22 @@
 package co.nimblehq.compose.ecommerce.ui.screen.product
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import co.nimblehq.compose.ecommerce.ui.theme.Purple600
+import androidx.compose.ui.unit.dp
+import co.nimblehq.compose.ecommerce.R
+import co.nimblehq.compose.ecommerce.ui.theme.AppTextStyle
 
 @Composable
 fun ProductScreen() {
@@ -21,16 +24,33 @@ fun ProductScreen() {
         modifier = Modifier
             .fillMaxSize()
             .background(color = Color.White)
-            .wrapContentSize(Alignment.Center)
     ) {
+
         Text(
-            text = "Product View",
-            fontWeight = FontWeight.Bold,
-            color = Purple600,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 25.sp
+            modifier = Modifier
+                .padding(top = 50.dp, bottom = 21.dp, start = 16.dp, end = 16.dp),
+            text = stringResource(R.string.product_my_cart),
+            style = AppTextStyle.title
         )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .align(Alignment.CenterHorizontally)
+                .wrapContentSize(Alignment.Center)
+        ) {
+            Image(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                painter = painterResource(id = R.drawable.ic_cart), contentDescription = null
+            )
+
+            Text(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 16.dp),
+                text = stringResource(id = R.string.product_empty_cart)
+            )
+        }
     }
 }
 
