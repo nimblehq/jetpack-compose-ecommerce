5c5a27db8c13fbf17e6b8c11150a6ad044451d9f
hoangnguyen92dn <hoang.l@nimblehq.co>
2022-04-09 11:27:49 +0700

    [#20] Update to change border color on color and size picker selected

--- app/src/main/java/co/nimblehq/compose/ecommerce/ui/product/ProductColor.kt
+++ app/src/main/java/co/nimblehq/compose/ecommerce/ui/product/ProductColor.kt
@@ -1,145 +1,114 @@
 package co.nimblehq.compose.ecommerce.ui.product
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.annotation.StringRes
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import co.nimblehq.compose.ecommerce.R
 import co.nimblehq.compose.ecommerce.ui.theme.*
 import co.nimblehq.compose.ecommerce.ui.theme.AppTextStyle.productSize
+import com.google.accompanist.flowlayout.FlowMainAxisAlignment
+import com.google.accompanist.flowlayout.FlowRow
+import com.google.accompanist.flowlayout.SizeMode
 
 @ExperimentalFoundationApi
 @Preview
 @Composable
 fun ProductColor() {
 
+    val selectedColor = remember { mutableStateOf(0) }
     Column(
         modifier = Modifier.padding(horizontal = 16.dp)
     ) {
         Text(
-            modifier = Modifier.align(Alignment.Start),
+            modifier = Modifier
+                .align(Alignment.Start)
+                .padding(bottom = 8.dp),
             text = stringResource(id = R.string.product_color),
             style = AppTextStyle.productDetailHeader
         )
 
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 8.dp),
+        val listOfColor = listOf(
+            Pink to R.string.product_color_pink,
+            Indigo to R.string.product_color_indigo,
+            Honey to R.string.product_color_honey,
+            Gray4 to R.string.product_color_gray,
+        )
+
+        val spacer = 8.dp
+        val itemMaxWidth =
+            (LocalConfiguration.current.screenWidthDp.dp - (16.dp * 2) - spacer) / 2
+        FlowRow(
+            mainAxisSize = SizeMode.Expand,
+            mainAxisAlignment = FlowMainAxisAlignment.SpaceEvenly
         ) {
-            val modifier = Modifier
-                .weight(1f)
-                .border(
-                    border = BorderStroke(1.dp, color = Purple700),
-                    shape = RoundedCornerShape(8.dp)
-                )
-                .padding(top = 14.dp, bottom = 14.dp)
+            listOfColor.forEachIndexed { index, (color, stringResId) ->
+                val borderColor = if (selectedColor.value == index) {
+                    Purple700
+                } else {
+                    Gray7
+                }
 
-            Column(
-                modifier = modifier,
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Box(
-                    modifier = Modifier
-                        .size(24.dp)
-                        .clip(CircleShape)
-                        .background(Pink)
-                )
+                val modifier = Modifier
+                    .requiredWidth(itemMaxWidth)
+                    .padding(bottom = 8.dp)
+                    .border(
+                        border = BorderStroke(1.dp, color = borderColor),
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .padding(top = 14.dp, bottom = 14.dp)
 
-                Text(
-                    modifier = Modifier.padding(top = 8.dp),
-                    text = stringResource(id = R.string.product_color_pink),
-                    style = productSize
-                )
-            }
-
-
-            Spacer(modifier = Modifier.width(8.dp))
-
-            Column(
-                modifier = modifier,
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Box(
-                    modifier = Modifier
-                        .size(24.dp)
-                        .clip(CircleShape)
-                        .background(Indigo)
+                ColorPicker(
+                    modifier = modifier,
+                    color = color,
+                    stringId = stringResId,
+                    onClick = { selectedColor.value = index }
                 )
 
-                Text(
-                    modifier = Modifier.padding(top = 8.dp),
-                    text = stringResource(id = R.string.product_color_indigo),
-                    style = productSize
-                )
+                if (index % 2 == 0) {
+                    Spacer(modifier = Modifier.width(8.dp))
+                }
             }
         }
+    }
+}
 
-        Row(
+@Composable
+fun ColorPicker(
+    modifier: Modifier,
+    color: Color,
+    @StringRes stringId: Int,
+    onClick: () -> Unit
+) {
+    Column(
+        modifier = modifier.clickable { onClick() },
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 8.dp),
-        ) {
-            val modifier = Modifier
-                .weight(1f)
-                .border(
-                    border = BorderStroke(1.dp, color = Purple700),
-                    shape = RoundedCornerShape(8.dp)
-                )
-                .padding(top = 14.dp, bottom = 14.dp)
+                .size(24.dp)
+                .clip(CircleShape)
+                .background(color)
+        )
 
-            Column(
-                modifier = modifier,
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Box(
-                    modifier = Modifier
-                        .size(24.dp)
-                        .clip(CircleShape)
-                        .background(Honey)
-                )
-
-                Text(
-                    modifier = Modifier.padding(top = 8.dp),
-                    text = stringResource(id = R.string.product_color_honey),
-                    style = productSize
-                )
-            }
-
-            Spacer(modifier = Modifier.width(8.dp))
-
-            Column(
-                modifier = modifier,
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Box(
-                    modifier = Modifier
-                        .size(24.dp)
-                        .clip(CircleShape)
-                        .background(Gray4)
-                )
-
-                Text(
-                    modifier = Modifier.padding(top = 8.dp),
-                    text = stringResource(id = R.string.product_color_gray),
-                    style = productSize
-                )
-            }
-        }
+        Text(
+            modifier = Modifier.padding(top = 8.dp),
+            text = stringResource(id = stringId),
+            style = productSize
+        )
     }
 }
--- app/src/main/java/co/nimblehq/compose/ecommerce/ui/product/ProductSize.kt
+++ app/src/main/java/co/nimblehq/compose/ecommerce/ui/product/ProductSize.kt
@@ -3,10 +3,13 @@ package co.nimblehq.compose.ecommerce.ui.product
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -15,6 +18,7 @@ import androidx.compose.ui.unit.dp
 import co.nimblehq.compose.ecommerce.R
 import co.nimblehq.compose.ecommerce.ui.theme.AppTextStyle
 import co.nimblehq.compose.ecommerce.ui.theme.AppTextStyle.productSize
+import co.nimblehq.compose.ecommerce.ui.theme.Gray7
 import co.nimblehq.compose.ecommerce.ui.theme.Purple700
 
 @ExperimentalFoundationApi
@@ -22,6 +26,7 @@ import co.nimblehq.compose.ecommerce.ui.theme.Purple700
 @Composable
 fun ProductSize() {
 
+    val selectedSize = remember { mutableStateOf(0) }
     Column(
         modifier = Modifier.padding(horizontal = 16.dp)
     ) {
@@ -36,43 +41,38 @@ fun ProductSize() {
                 .fillMaxWidth()
                 .padding(top = 8.dp),
         ) {
-            val modifier = Modifier
-                .weight(1f)
-                .border(
-                    border = BorderStroke(1.dp, color = Purple700),
-                    shape = RoundedCornerShape(8.dp)
+            val listOfSize = listOf(
+                R.string.product_size_s,
+                R.string.product_size_m,
+                R.string.product_size_l,
+                R.string.product_size_xl,
+            )
+
+            listOfSize.forEachIndexed { index, stringResId ->
+                val borderColor = if (selectedSize.value == index) {
+                    Purple700
+                } else {
+                    Gray7
+                }
+                Text(
+                    modifier = Modifier
+                        .weight(1f)
+                        .border(
+                            border = BorderStroke(1.dp, color = borderColor),
+                            shape = RoundedCornerShape(8.dp)
+                        )
+                        .padding(top = 14.dp, bottom = 14.dp)
+                        .clickable {
+                            selectedSize.value = index
+                        },
+                    text = stringResource(id = stringResId),
+                    style = productSize
                 )
-                .padding(top = 14.dp, bottom = 14.dp)
 
-            Text(
-                modifier = modifier,
-                text = stringResource(id = R.string.product_size_s),
-                style = productSize
-            )
-
-            Spacer(modifier = Modifier.width(8.dp))
-
-            Text(
-                modifier = modifier,
-                text = stringResource(id = R.string.product_size_m),
-                style = productSize
-            )
-
-            Spacer(modifier = Modifier.width(8.dp))
-
-            Text(
-                modifier = modifier,
-                text = stringResource(id = R.string.product_size_l),
-                style = productSize
-            )
-
-            Spacer(modifier = Modifier.width(8.dp))
-
-            Text(
-                modifier = modifier,
-                text = stringResource(id = R.string.product_size_xl),
-                style = productSize
-            )
+                if (index < listOfSize.lastIndex) {
+                    Spacer(modifier = Modifier.width(8.dp))
+                }
+            }
         }
     }
 }
